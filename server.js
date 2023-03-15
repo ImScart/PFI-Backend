475ebaf16b96c04be834a701c1f4c1bf12f5d488
Mathieu Chabot <scartcontact@gmail.com>
2023-03-15 13:13:21 -0400

    Added secure Git setup

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const express = require('express');
-console.log(express);
+const keys = require('./config/keys.js');
+
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+// Routes
+app.get('/auth', async(req, res) => {
+console.log(req.query);
+res.send("Hello World! It is " + Date.now());
+});
+
+app.listen(keys.port, () => {
+    console.log("Listeting on " + keys.port);
+});
