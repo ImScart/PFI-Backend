3ede3e05e10ab0ca33c43b84db2c8054ac19e5d4
Mathieu Chabot <scartcontact@gmail.com>
2023-03-15 20:38:33 -0400

    Now connecting to database and storing data.

--- server.js
+++ server.js
@@ -8,15 +8,49 @@ const mongoose = require('mongoose');
 mongoose.connect(keys.mongoURI);
 // Setup database models
 require('./model/Account');
+const Account = mongoose.model('accounts');
 
 // Routes
 app.get('/account', async(req, res) => {
+    const {rUsername, rPassword} = req.query;
+    if(rUsername ==null || rPassword == null)
+    {
+        res.send("Missing username or password.");
+        return;
+    }
 
-    console.log(req.query);
-    const {username, password} = req.query;
-    console.log(username);
-    console.log(password);
-    res.send('hello');
+    var userAccount = await Account.findOne({username: rUsername})
+    if(userAccount == null)
+    {
+        // Create a new user
+        console.log("Creating a new account...")
+        {
+            var newAccount = new Account({
+                username : rUsername,
+                password : rPassword,
+
+                lastAuthentication : Date.now()
+            });
+            await newAccount.save();
+
+            res.send(newAccount);
+            return;
+        }
+    }
+    else
+    {
+        if(rPassword == userAccount.password)
+        {
+            console.log("retrieving account")
+            userAccount.lastAuthentication = Date.now();
+            await userAccount.save();
+            res.send(userAccount);
+        }
+        else
+        {
+            res.send('Invalid password');
+        }
+    }
 
 });
 
